a94a1aac9c6a9ceb0d07cebfdaa03ceb85e8dd6b
Angel Torres <alas4224@gmail.com>
2019-01-23 17:38:09 -0600

    added styles with bootstrap

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -7,14 +7,14 @@ export default class FriendForm extends Component {
       <div className="form-wrapper">
         <form className="form-wrapper">
             <div>
-                <input className="input" type="text" placeholder="name"/>
-                <input className="input" type="text" placeholder="age"/>
-                <input className="input" type="text" placeholder="email"/>
+            <input className="form-control form-control-lg" type="text" placeholder="name"/>
+            <input className="form-control form-control-lg" type="text" placeholder="age"/>
+            <input className="form-control form-control-lg" type="email" placeholder="email"/>
             </div>
             <div>
-                <button>Add Friend</button>
-                <button>Update Friend</button>
-                <button>Delete Friend</button>
+                <button className="btn btn-success">Add Friend</button>
+                <button className="btn btn-info">Update Friend</button>
+                <button className="btn btn-danger">Delete Friend</button>
             </div>
         </form>
       </div>
--- friends/src/components/FriendsDisplay.js
+++ friends/src/components/FriendsDisplay.js
@@ -9,9 +9,18 @@ export default class FriendsDisplay extends Component {
     render() {
         console.log(this.props)
         return (
-        <div className="friend-wrapper">
+        <div className="list-group">
             {this.props.data.map( friend => (
-                <div className="friend-card">
+                // <div className="card">
+                //     <h5 className="card-header">{friend.name}</h5>
+                //     <div className="card-body">
+                //         <h5 className="card-title">Age: {friend.age}</h5>
+                //         <p className="card-text">Email: {friend.email}</p>
+                //         <a href="#" className="btn btn-primary">Go somewhere</a>
+                //     </div>
+                //  </div>
+                 
+                <div className="list-group-item">
                     <h1>{friend.name}</h1>
                     <p>Age: {friend.age}</p>
                     <p>Email: {friend.email}</p>
